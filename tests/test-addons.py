7a100c5d7d2009cd9d2b18c3e5476c0c3b40ea35
Madalin Ignisca <git@madalin.me>
2022-06-03 14:13:56 +0200

    Fix testing for OpenEBS

--- tests/test-addons.py
+++ tests/test-addons.py
@@ -8,7 +8,13 @@ class TestAddons(object):
     def test_openebs(self):
         microk8s_enable("openebs")
         wait_for_pod_state(
-            "", "openebs", "running", label="app=openebs-nfs-provisioner"
+            "", "openebs", "running", label="app=openebs"
+        )
+        wait_for_pod_state(
+            "", "openebs", "running", label="component=nfs-provisioner"
+        )
+        wait_for_pod_state(
+            "", "openebs", "running", label="component=jiva-operator"
         )
         status = yaml.safe_load(sh.microk8s.status(format="yaml").stdout)
         expected = {"openebs": "enabled"}
