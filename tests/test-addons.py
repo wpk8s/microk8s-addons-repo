db986d227f28d9ce3ec95a630fa0d90124e9356a
Madalin Ignisca <git@madalin.me>
2022-06-03 12:57:11 +0200

    Change namespace for expected container of OpenEBS

--- tests/test-addons.py
+++ tests/test-addons.py
@@ -8,7 +8,7 @@ class TestAddons(object):
     def test_openebs(self):
         microk8s_enable("openebs")
         wait_for_pod_state(
-            "", "default", "running", label="app=openebs-nfs-provisioner"
+            "", "openebs", "running", label="app=openebs-nfs-provisioner"
         )
         status = yaml.safe_load(sh.microk8s.status(format="yaml").stdout)
         expected = {"openebs": "enabled"}
