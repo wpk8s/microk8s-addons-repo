3a15fac941ccca6a6b38d6d84243f22e22453fec
Madalin Ignisca <git@madalin.me>
2022-06-03 14:16:12 +0200

    Fix formatting

--- tests/test-addons.py
+++ tests/test-addons.py
@@ -1,21 +1,15 @@
 import sh
 import yaml
 
-from utils import microk8s_enable, wait_for_pod_state, microk8s_disable
+from utils import kubectl_get, microk8s_enable, wait_for_pod_state, microk8s_disable
 
 
 class TestAddons(object):
     def test_openebs(self):
         microk8s_enable("openebs")
-        wait_for_pod_state(
-            "", "openebs", "running", label="app=openebs"
-        )
-        wait_for_pod_state(
-            "", "openebs", "running", label="component=nfs-provisioner"
-        )
-        wait_for_pod_state(
-            "", "openebs", "running", label="component=jiva-operator"
-        )
+        wait_for_pod_state("", "openebs", "running", label="app=openebs")
+        wait_for_pod_state("", "openebs", "running", label="component=nfs-provisioner")
+        wait_for_pod_state("", "openebs", "running", label="component=jiva-operator")
         status = yaml.safe_load(sh.microk8s.status(format="yaml").stdout)
         expected = {"openebs": "enabled"}
         microk8s_disable("openebs")
