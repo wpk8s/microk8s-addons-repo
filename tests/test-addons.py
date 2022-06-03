dba6110ffe1325cfb4f8beed08ea66fafb4ce8df
Madalin Ignisca <git@madalin.me>
2022-06-03 10:02:38 +0000

    Used black to reformat

--- tests/test-addons.py
+++ tests/test-addons.py
@@ -7,7 +7,9 @@ from utils import microk8s_enable, wait_for_pod_state, microk8s_disable
 class TestAddons(object):
     def test_openebs(self):
         microk8s_enable("openebs")
-        wait_for_pod_state("", "default", "running", label="app=openebs-nfs-provisioner")
+        wait_for_pod_state(
+            "", "default", "running", label="app=openebs-nfs-provisioner"
+        )
         status = yaml.safe_load(sh.microk8s.status(format="yaml").stdout)
         expected = {"openebs": "enabled"}
         microk8s_disable("openebs")
